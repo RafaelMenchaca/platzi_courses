23793b112a9c0ed83348ebc675cdeecb24251e39
Rafael Menchaca <arafael.menchaca@gmail.com>
2026-01-15 21:00:03 -0600

    Curso OpenAI API: update en platzivision se agrega openai key por env

--- desarrollo_de_aplicaciones_con_ia/2_curso_de_openai_api/platzi_vision/platzivision-main/platzi-vision-api/app.py
+++ desarrollo_de_aplicaciones_con_ia/2_curso_de_openai_api/platzi_vision/platzivision-main/platzi-vision-api/app.py
@@ -3,5 +3,9 @@ import os
 
 app = create_app()
 
+@app.route('/')
+def index():
+    return "Welcome to the Platzi Vision API! Backend is running."
+
 if __name__ == '__main__':
     app.run(debug=True)
--- desarrollo_de_aplicaciones_con_ia/2_curso_de_openai_api/platzi_vision/platzivision-main/platzi-vision-api/app/api/chat.py
+++ desarrollo_de_aplicaciones_con_ia/2_curso_de_openai_api/platzi_vision/platzivision-main/platzi-vision-api/app/api/chat.py
@@ -2,6 +2,12 @@ from flask import request, jsonify, Response
 import openai
 import json
 from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 
 def generate_image(prompt: str, quality: str = "standard") -> str:
@@ -57,7 +63,7 @@ def chat():
                     "content": message["content"],
                 })
         
-        client = OpenAI()
+        client = OpenAI(api_key=OPENAI_API_KEY)
 
         tools = [
             {
