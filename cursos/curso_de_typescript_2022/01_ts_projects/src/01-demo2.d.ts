a92fd1463cb92a12ac77757e6ca4f635e48dd011
Rafael Menchaca <arafael.menchaca@gmail.com>
2026-05-02 13:37:06 -0500

    curso de typescript: archivo tsconfig

---
+++ cursos/curso_de_typescript_2022/01_ts_projects/src/01-demo2.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=01-demo2.d.ts.map
---
+++ cursos/curso_de_typescript_2022/01_ts_projects/src/01_hello.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const myName = "Rafael"
+// console.log(myName)
+//# sourceMappingURL=01_hello.js.map
