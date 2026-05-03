f2a134d095588b463e9c1fa7a2d12c07ae514eb1
Rafael Menchaca <arafael.menchaca@gmail.com>
2026-05-03 12:37:44 -0500

    curso de typescript: numbers

---
+++ cursos/curso_de_typescript_2022/01_ts_projects/src/03_typing.ts
@@ -0,0 +1,19 @@
+(() => {
+
+    let myProductName = "Product 1";
+    let myProductPrice = 123;
+
+    // let myProductName: string = "Product 1";
+    // let myProductPrice: number = 123;
+
+    myProductName = "change";
+    myProductName.toLocaleLowerCase();
+
+
+    myProductPrice.toFixed();
+
+    const myProductStock = 1000;
+    const myProductName2 = "Product 1" 
+
+})();
+
